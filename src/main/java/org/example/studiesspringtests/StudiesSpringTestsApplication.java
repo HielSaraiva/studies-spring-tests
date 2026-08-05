54a8d1f78dcf170e2ac85de927086c06d5118018
Hiel Saraiva <hielsaraiva11.hs@gmail.com>
2026-08-04 21:32:05 -0300

    feat: creating project

---
+++ src/main/java/org/example/studiesspringtests/StudiesSpringTestsApplication.java
@@ -0,0 +1,13 @@
+package org.example.studiesspringtests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudiesSpringTestsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudiesSpringTestsApplication.class, args);
+    }
+
+}
